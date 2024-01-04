60cac88d0384483017e990eebb841236b93b0837
Bartek Kociuba <bar650@gmail.com>
2024-01-04 20:45:36 +0100

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public FizzBuzz()
+        {    
+        }
+        public string FizzBuzzMethod(int number)
+        {
+            
+
+                if ((number % 3 == 0) && (number % 5 == 0))
+                {
+                    Console.WriteLine("FizBuzz");
+                    
+                }
+                else
+                if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    
+                }
+                else
+                if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                    
+                }
+                return number.ToString();
+            
+            
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //user wprowadza liczbę
+            Console.WriteLine("Jesteś w grze FizzBuzz.\n Fizz za 3-ke,\n Buzz za 5-tkę,\n FizzBuzz za 3-kę z 5-tką.\n\n Podaj liczbę, aby zacząć rozdanie: ");
+            var numberUser = GetNumber();
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.FizzBuzzMethod(numberUser);
+        }
+        static int GetNumber()
+        {
+            while(true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podałes złą wartość, wpisz ponownie");
+                    continue;
+                }
+                return number;
+            }
+            
+        }
+    }
+}
